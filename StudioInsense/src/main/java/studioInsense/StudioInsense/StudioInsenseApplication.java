e497c0d6f310d9ebe539b5ef98ff106a387a11e2
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2026-03-05 21:23:43 +0100

    create springboot project

---
+++ StudioInsense/src/main/java/studioInsense/StudioInsense/StudioInsenseApplication.java
@@ -0,0 +1,13 @@
+package studioInsense.StudioInsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioInsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioInsenseApplication.class, args);
+	}
+
+}
---
+++ StudioInsense/src/test/java/studioInsense/StudioInsense/StudioInsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package studioInsense.StudioInsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioInsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

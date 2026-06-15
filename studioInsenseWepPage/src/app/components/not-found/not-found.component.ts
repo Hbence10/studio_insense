8cc35655e7c9c03757f0a4718705901c6f5612d6
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2026-06-15 07:03:55 +0200

    06.15.munkálatok#1

--- studioInsenseWepPage/src/app/components/not-found/not-found.component.ts
+++ studioInsenseWepPage/src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FrontendService } from '../../services/frontend.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
--- studioInsenseWepPage/src/app/components/unauthorized/unauthorized.component.ts
+++ studioInsenseWepPage/src/app/components/unauthorized/unauthorized.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FrontendService } from '../../services/frontend.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './unauthorized.component.html',
   styleUrl: './unauthorized.component.scss',
 })
